d0889252789bd083e03e6ab9c395e473b5f43478
sihrong <sihrong.cheng@leishan.com.tw>
2024-04-17 17:40:28 +0800

    feat:final

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import list from "../src/list.js";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
   const [isDefault, setIsDefault] = useState(true);
@@ -11,83 +11,111 @@ function App() {
   // const listLength = Object.keys(list).length;
   const [num, setNum] = useState(1);
 
+  // function random() {
+  //   const listLength = Object.keys(list).length;
+  //   setInterval(() => {
+  //     setNum((prevNum) => {
+  //       return Math.floor(Math.random() * listLength) + 1;
+  //       // if (prevNum >= listLength) {
+  //       //   return 1;
+  //       // } else {
+  //       //   return prevNum + 1;
+  //       // }
+  //     });
+  //   }, 100);
+  // }
+
+  const intervalRef = useRef(null); // 用于存储 setInterval 的引用
+
   function random() {
     const listLength = Object.keys(list).length;
-    setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setNum((prevNum) => {
-        if (prevNum >= listLength) {
-          return 1;
-        } else {
-          return prevNum + 1;
-        }
+        return Math.floor(Math.random() * listLength) + 1;
       });
-    }, 200);
+    }, 100);
+  }
+
+  function stop() {
+    setIsActive(false);
+    clearInterval(intervalRef.current); // 清除定时器
   }
 
   function start() {
     setIsDefault(false);
     setIsActive(true);
     random();
+    setTimeout(() => {
+      stop();
+    }, 8000);
   }
+
+  function restart() {
+    setIsDefault(true);
+  }
+
+  const buttonSx = {
+    marginTop: 3,
+    width: 200,
+    height: 60,
+    fontSize: 24,
+  };
 
   return (
     <>
-      <div className="App flex-col align-middle">
-        {isDefault ? (
-          <Box
-            className="mx-auto"
-            height={"60vh"}
-            width={"60vh"}
-            my={4}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            gap={10}
-            p={"auto"}
-            sx={{ border: "2px solid grey" }}
-          >
+      <div className="App flex justify-center mt-7">
+        <Box
+          height={"90vh"}
+          width={"80vh"}
+          // my={4}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          gap={10}
+          p={"auto"}
+          sx={{ border: "2px solid grey" }}
+        >
+          {isDefault ? (
             <p className="text-[50px]">生日好禮抽抽抽</p>
-          </Box>
-        ) : (
-          // <div className="h-[60vh]  h-96 bg-[red] mx-auto my-4 p-[70px]">
-          //   生日好禮抽抽抽
-          // </div>
-          <Box
-            className="mx-auto"
-            height={"60vh"}
-            width={"60vh"}
-            my={4}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            gap={10}
-            p={"auto"}
-            sx={{ border: "2px solid grey" }}
-          >
+          ) : (
             <img
               className="max-h-[100%] w-auto"
               src={process.env.PUBLIC_URL + `/photo/${num}.jpg`}
               alt={num}
             />
-          </Box>
-          // <div className=" w-[500px] h-[60vh] bg-[red] mx-auto my-4 flex-col content-between">
-          //   <div>
-          //     <img
-          //       width="100%"
-          //       src={process.env.PUBLIC_URL + `/photo/${num}.jpg`}
-          //       alt={num}
-          //     />
-          //   </div>
-          //   <p>{list[num]}</p>
-          // </div>
-        )}
-        {!isActive ? (
-          <Button variant="contained" onClick={start} size="large">
-            開始抽獎
-          </Button>
-        ) : (
-          ""
-        )}
+          )}
+        </Box>
+
+        <div className="w-[20vw] my-auto">
+          {" "}
+          <p className="text-[45px] ml-2">
+            {!isActive && !isDefault ? `恭喜${list[num]}!` : " "}
+          </p>
+          {!isActive && isDefault ? (
+            <Button
+              variant="contained"
+              onClick={start}
+              size="large"
+              sx={buttonSx}
+            >
+              開始抽獎
+            </Button>
+          ) : (
+            ""
+          )}
+          {!isActive && !isDefault ? (
+            <Button
+              variant="contained"
+              onClick={restart}
+              size="large"
+              sx={buttonSx}
+            >
+              重新開始
+            </Button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </>
   );
